fad7ee792a98b2bcba55cd90653f602e6020bdb1
Chenghao Hou <houchenghao@hotmail.com>
2023-04-16 02:15:53 +1000

    fix package.json spelling issue

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,7 +18,7 @@ export const putDb = async (content) => {
     const jateDB = await openDB("jate", 1);
     const tx = jateDB.transaction("jate", "readwrite");
     const store = tx.objectStore("jate");
-    const request = store.put({ jate: content });
+    const request = store.put({ id:1, value: content });
     const result = await request;
     console.log("Data saved to the database", result);
   }catch(err){
@@ -33,7 +33,7 @@ export const getDb = async () => {
     const jateDB = await openDB("jate", 1);
     const tx = jateDB.transaction("jate", "readonly");
     const store = tx.objectStore("jate");
-    const request = store.getAll();
+    const request = store.get(1);
     const result = await request;
     console.log(result);
     return result;
